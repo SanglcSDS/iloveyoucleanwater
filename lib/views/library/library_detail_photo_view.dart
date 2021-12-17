80afd6a67d4746cad8f27ec505fbd1584ec953c9
sanglcsds <sanglc@mitec.com.vn>
2021-12-17 09:50:38 +0700

    sanglc

--- lib/views/library/library_detail_photo_view.dart
+++ lib/views/library/library_detail_photo_view.dart
@@ -1,77 +1,205 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:get/get.dart';
-import 'package:iloveyoucleanwater/models/library/library_video_model.dart';
+import 'package:iloveyoucleanwater/models/library/library_detail_photo_model.dart';
 
 import 'package:iloveyoucleanwater/utils/constants.dart';
-import 'package:iloveyoucleanwater/views/library/library_video_view.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:iloveyoucleanwater/views/shared/widgets/circle_button.dart';
+import 'package:shimmer/shimmer.dart';
 
-class ItemVideoView extends StatelessWidget {
-  YoutubePlayerController videoController;
-  LibraryVideoModel LibraryVideo;
-  ItemVideoView({required this.videoController, required this.LibraryVideo});
+class LibraryDetailPhotoView extends StatelessWidget {
+  final LibraryDetailPhotoModel news;
+  final String title;
+  final String datetime;
+  LibraryDetailPhotoView(
+      {required this.news, required this.title, required this.datetime});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MediaQuery.of(context).orientation == Orientation.portrait
-          ? Stack(
-              children: [
-                Column(
-                  children: <Widget>[
-                    SafeArea(
-                      child: SizedBox(
-                          child: YoutubePlayer(
-                        controller: videoController,
-                      )),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 12.0, vertical: 12.0),
-                      child: Text(
-                        LibraryVideo.title,
-                        textAlign: TextAlign.justify,
-
-                        //maxLines: 2,
-                        style: kTitleCard1,
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(65.0),
+        child: Center(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(18.0, 15.0, 18.0, 0),
+              child: Row(
+                children: [
+                  CircleButton(
+                    colors: kGrey1,
+                    icon: Icons.arrow_back_ios,
+                    onTap: () => Get.back(),
+                  ),
+                  const Spacer(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+        child: ListView(
+          children: [
+            const SizedBox(height: 12.0),
+            Container(
+              height: 220.0,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(5),
+                    topLeft: Radius.circular(5),
+                    bottomLeft: Radius.circular(5),
+                    bottomRight: Radius.circular(5)),
+                child: CachedNetworkImage(
+                  imageUrl: (news.image.contains(Constants.URL_IMAGE)
+                      ? news.image
+                      : Constants.URL_IMAGE + news.image),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      Center(
+                    child: Shimmer.fromColors(
+                      baseColor: Colors.grey[400]!,
+                      highlightColor: Colors.grey[100]!,
+                      child: Container(
+                        width: double.infinity,
+                        height: double.infinity,
+                        color: Colors.white,
                       ),
                     ),
-                    Container(
-                      decoration: const BoxDecoration(
-                        border: Border(
-                          top: BorderSide(
-                            width: 5.0,
-                            color: kGrey3,
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+
+              // DecorationImage(
+              //   image: (news.image.contains(Constants.URL_IMAGE)
+              //       ? news.image
+              //       : Constants.URL_IMAGE + news.image),
+              //   fit: BoxFit.cover,
+              // ),
+            ),
+            const SizedBox(height: 15.0),
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12.0,
+                    vertical: 15.0,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: kGrey3, width: 1.0),
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  child: Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 5.0,
+                        backgroundColor: kGrey3,
+                      ),
+                      const SizedBox(width: 6.0),
+                      Text(
+                        title,
+                        style: kCategoryTitle,
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                Status(
+                  icon: Icons.remove_red_eye,
+                  total: news.view.toString(),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12.0),
+            Text(
+              news.title,
+              style: kTitleCard,
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(height: 15.0),
+            Row(
+              children: [
+                const SizedBox(width: 3.0),
+                const Icon(
+                  Icons.date_range,
+                  color: kGrey1,
+                ),
+                const SizedBox(width: 5.0),
+                Expanded(
+                    child: Text(
+                        calculateTimeDifferenceBetween(stringTime: datetime),
+                        style: kDetailContent,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1))
+              ],
+            ),
+            const SizedBox(height: 15.0),
+            Html(
+              data: news.content,
+              style: {
+                "p": Style(
+                  textAlign: TextAlign.justify,
+                ),
+                "img": Style(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    height: MediaQuery.of(context).size.height * 0.7)
+              },
+            ),
+            const SizedBox(height: 15.0),
+            Column(
+              children: List.generate(news.listImages.length, (index) {
+                var image = news.listImages[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5.0),
+                  child: Container(
+                    height: 220.0,
+                    child: CachedNetworkImage(
+                      imageUrl: (image.link.contains(Constants.URL_IMAGE)
+                          ? image.link
+                          : Constants.URL_IMAGE + image.link),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                      progressIndicatorBuilder:
+                          (context, url, downloadProgress) => Center(
+                        child: Shimmer.fromColors(
+                          baseColor: Colors.grey[400]!,
+                          highlightColor: Colors.grey[100]!,
+                          child: Container(
+                            width: double.infinity,
+                            height: double.infinity,
+                            color: Colors.white,
                           ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: LibraryVideoView(
-                        tag: "sss",
-                      ),
-                    )
-                  ],
-                ),
-                SafeArea(
-                  child: Container(
-                    alignment: Alignment.topLeft,
-                    padding: const EdgeInsets.only(left: 5, top: 5),
-                    child: IconButton(
-                      icon: const Icon(Icons.arrow_back_ios, color: kGrey2),
-                      onPressed: () => Get.back(),
+                      fit: BoxFit.cover,
                     ),
                   ),
-                ),
-              ],
-            )
-          : SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: YoutubePlayer(
-                controller: videoController,
-              ),
+                );
+              }),
             ),
+            const SizedBox(height: 25.0)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Status extends StatelessWidget {
+  final IconData icon;
+  final String total;
+  Status({required this.icon, required this.total});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon, color: kGrey2),
+        const SizedBox(width: 4.0),
+        Text(total, style: kDetailContent),
+      ],
     );
   }
 }
